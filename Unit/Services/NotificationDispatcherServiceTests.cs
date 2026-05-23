5c86a5aa14e086022f7346755db1a0005d36dd68
Eduardo Luis Mexia Angulo <eduardo.mexia@sofiaxt.com>
2026-05-23 15:19:59 -0700

    added more dispatcher tests

--- Unit/Services/NotificationDispatcherServiceTests.cs
+++ Unit/Services/NotificationDispatcherServiceTests.cs
@@ -22,7 +22,7 @@ namespace Unit.Services
             _channelMock = new();
 
         [Fact]
-        public async Task DispatchAsync_Should_Send_Notifications()
+        public async Task DispatchAsync_Should_Send_Notifications_One_Channel()
         {
             // Arrange
             var user = new User
@@ -84,6 +84,108 @@ namespace Unit.Services
                 x => x.CreateAsync(
                     It.IsAny<NotificationLog>()),
                 Times.Once);
+        }
+
+        [Fact]
+        public async Task DispatchAsync_Should_Send_Notifications_Three_Channel()
+        {
+            // Arrange
+            var user = new User
+            {
+                Id = 1,
+                Name = "Eduardo",
+                Email = "eduardo@test.com",
+
+                Channels = new List<Channel>
+            {
+                new Channel
+                {
+                    Id = (int)NotificationChannelType.Email,
+                    Name = "Email",
+                    ChannelType = NotificationChannelType.Email
+                },
+                new Channel
+                {
+                    Id = (int)NotificationChannelType.Sms,
+                    Name = "SMS",
+                    ChannelType = NotificationChannelType.Sms
+                },
+                new Channel
+                {
+                    Id = (int)NotificationChannelType.Push,
+                    Name = "Push",
+                    ChannelType = NotificationChannelType.Push 
+                }
+            }
+            };
+
+            var message = new Message
+            {
+                Id = 1,
+                Body = "Test notification",
+                CategoryId = 1,
+                CorrelationId = Guid.NewGuid()
+            };
+
+            _userRepositoryMock
+                .Setup(x => x.GetSubscribedUsersAsync(
+                    message.CategoryId))
+                .ReturnsAsync(new List<User> { user });
+
+            //channel mocks
+            Mock<INotificationChannel> emailChannelMock = new();
+            emailChannelMock
+                .Setup(x => x.ChannelType)
+                .Returns(NotificationChannelType.Email);
+
+            emailChannelMock
+                .Setup(x => x.SendAsync(user, message))
+                .ReturnsAsync(NotificationResult.Successful());
+
+            Mock<INotificationChannel> smsChannelMock = new();
+            smsChannelMock
+                .Setup(x => x.ChannelType)
+                .Returns(NotificationChannelType.Sms);
+            smsChannelMock
+                .Setup(x => x.SendAsync(user, message))
+                .ReturnsAsync(NotificationResult.Successful());
+
+            Mock<INotificationChannel> pushChannelMock = new();
+            pushChannelMock
+                .Setup(x => x.ChannelType)
+                .Returns(NotificationChannelType.Push);
+            pushChannelMock 
+                .Setup(x => x.SendAsync(user, message))
+                .ReturnsAsync(NotificationResult.Successful());
+
+            var service = new NotificationDispatcherService(
+                _userRepositoryMock.Object,
+                _logRepositoryMock.Object,
+                new List<INotificationChannel>
+                {
+                    emailChannelMock.Object, pushChannelMock.Object, smsChannelMock.Object
+                });
+
+            // Act
+            await service.DispatchAsync(message);
+
+            // Assert
+            emailChannelMock.Verify(
+                x => x.SendAsync(user, message),
+                Times.Once);
+
+            smsChannelMock.Verify(
+                x => x.SendAsync(user, message),
+                Times.Once);
+
+            pushChannelMock.Verify(
+                x => x.SendAsync(user, message),
+                Times.Once);
+
+            _logRepositoryMock.Verify(
+                x => x.CreateAsync(
+                    It.IsAny<NotificationLog>()),
+                Times.Exactly(3));
         }
 
         [Fact]
